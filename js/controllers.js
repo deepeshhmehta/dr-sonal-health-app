5eeca046f67b81ca14d835b33261078db3420dba
deepeshmehta <deepeshhmehta@gmail.com>
2017-02-21 15:18:25 +0530

    version updated -- shared record bug fixed -- last support update for hope

--- js/controllers.js
+++ js/controllers.js
@@ -1875,7 +1875,7 @@ angular.module('PasswordConfirm', []).directive('changePasswordC', function () {
             $scope.limit = 3;
             $scope.recId = [];
             $scope.recIds = [];
-            $scope.userId = get('id');
+            $scope.userId = get('doctorid');
             $scope.patientId = get('id');
             $scope.repeatFreq = [];
             $scope.repeatNo = [];
